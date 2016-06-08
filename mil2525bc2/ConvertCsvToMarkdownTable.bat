f1eff66d8850e5cfcf62d749a1f7c2e6127d054d
Chris Moore <csmooreacm@acm.org>
2016-06-08 12:35:35 -0400

    Add Bc2

---
+++ mil2525bc2/ConvertCsvToMarkdownTable.bat
@@ -0,0 +1,10 @@
+REM SET Below to Python Path on your machine
+
+REM Command Format: python {PYTHONFILE}.py [Params] > [log file]
+
+SET PYTHON_PATH="C:\Python27\ArcGIS10.3"
+
+%PYTHON_PATH%\python ..\utilities\csv2markdownTable.py Mil2525Bc2-Test-Results.csv Mil2525Bc2-Test-Results.md
+
+echo Done!
+pause
